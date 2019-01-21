92a9d90110a7b63bb802a392579f59a111c9f857
hafizjoundy <hafizjoundys@gmail.com>
2019-01-21 23:41:50 +0700

    update :D

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Home from './components/Home'
+// import Testing from './components/Testing'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Blog under maintenance, -jon
-          </p>
-          <a
-            className="App-link"
-            href="https://fb.com/haffjjj"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            fb
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import "./app.css"
 
-export default App;
+const About = () => <h2>About</h2>
+
+const AppRouter = () => (
+  <Router>
+    <div>
+
+      {/* <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about/">About</Link>
+          </li>
+          <li>
+            <Link to="/users/">Users</Link>
+          </li>
+        </ul>
+      </nav> */}
+
+      <Route path="/" exact component={Home} />
+      {/* <Route path="/" exact component={Testing} /> */}
+      <Route path="/about/" component={About} />
+
+    </div>
+  </Router>
+)
+
+export default AppRouter
